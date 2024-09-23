6b3c45e428e201aad79843ccbad7f5bd32f020a0
stillwarter <2406017193@qq.com>
2024-09-23 21:54:42 +0800

    sitllwarter-add eleforge

---
+++ electron/eleForge/vueStart/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <mainLayout />
+</template>
+
+<script setup>
+import mainLayout from "./view/layout/mainLayout.vue";
+</script>
---
+++ electron/eleForge/vueStart/src/view/layout/mainLayout.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>
+    this is layout  
+  </div>
+</template>
